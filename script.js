2396ff570b9c5f02d8ef6f1be23f00d9409a67b9
CJ-03 <luvery03@outlook.com>
2020-03-15 13:15:08 -0600

    date

--- script.js
+++ script.js
@@ -1,66 +1,10 @@
 $(function() {
 
     $.ajax({
-        url: 'https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF43718?token=8fdb779315f6a04ff358eb03287ee823ee2473623a3c4a1797b814d75be8c1d8',
+        url: 'https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF43718/datos?token=8fdb779315f6a04ff358eb03287ee823ee2473623a3c4a1797b814d75be8c1d8',
         success: function(data) {
 
             console.log(data);
-/*
-            Highcharts.chart('container', {
-                chart: {
-                    zoomType: 'x'
-                },
-                title: {
-                    text: 'USD to EUR exchange rate over time'
-                },
-                subtitle: {
-                    text: document.ontouchstart === undefined ?
-                        'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
-                },
-                xAxis: {
-                    type: 'datetime'
-                },
-                yAxis: {
-                    title: {
-                        text: 'Exchange rate'
-                    }
-                },
-                legend: {
-                    enabled: false
-                },
-                plotOptions: {
-                    area: {
-                        fillColor: {
-                            linearGradient: {
-                                x1: 0,
-                                y1: 0,
-                                x2: 0,
-                                y2: 1
-                            },
-                            stops: [
-                                [0, Highcharts.getOptions().colors[0]],
-                                [1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                            ]
-                        },
-                        marker: {
-                            radius: 2
-                        },
-                        lineWidth: 1,
-                        states: {
-                            hover: {
-                                lineWidth: 1
-                            }
-                        },
-                        threshold: null
-                    }
-                },
-
-                series: [{
-                    type: 'area',
-                    name: 'USD to EUR',
-                    data: data
-                }]
-            });*/
         }
     });
 })
@@ -265,3 +209,60 @@ Highcharts.theme = {
 };
 
 Highcharts.setOptions(Highcharts.theme);
+/*
+Highcharts.chart('container', {
+    chart: {
+        zoomType: 'x'
+    },
+    title: {
+        text: 'USD to EUR exchange rate over time'
+    },
+    subtitle: {
+        text: document.ontouchstart === undefined ?
+            'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+    },
+    xAxis: {
+        type: 'datetime'
+    },
+    yAxis: {
+        title: {
+            text: 'Exchange rate'
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    plotOptions: {
+        area: {
+            fillColor: {
+                linearGradient: {
+                    x1: 0,
+                    y1: 0,
+                    x2: 0,
+                    y2: 1
+                },
+                stops: [
+                    [0, Highcharts.getOptions().colors[0]],
+                    [1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                ]
+            },
+            marker: {
+                radius: 2
+            },
+            lineWidth: 1,
+            states: {
+                hover: {
+                    lineWidth: 1
+                }
+            },
+            threshold: null
+        }
+    },
+
+    series: [{
+        type: 'area',
+        name: 'USD to EUR',
+        data: data
+    }]
+});
+*/
